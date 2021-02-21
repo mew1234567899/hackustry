df6e8cdb6b1f08bf3071045dfaa121ef622f06f9
mew1234567899 <iamaperson123thetwelve@gmail.com>
2021-02-21 11:06:12 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,8 @@ require("content/unlock");
 require("content/blocks");
 require("content/turrets");
 // h require("content/cursed");
+const mapVariable = extend(SectorPreset, "mewsmap", Planet.serpulo, 174 , {
+    localizedName: "sadland",
+    description: "youll never smile again",
+    difficulty: 1
+})
